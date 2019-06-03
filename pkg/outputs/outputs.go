777bc562114fdbcdee8f05bd333939bf6d2cf218
Erick Daniszewski <edaniszewski@gmail.com>
2019-06-03 08:00:07 -0400

    start to update outputs

--- pkg/outputs/outputs.go
+++ pkg/outputs/outputs.go
@@ -1,6 +1,6 @@
 package outputs
 
-import "github.com/vapor-ware/synse-sdk/sdk"
+import "github.com/vapor-ware/synse-sdk/sdk/output"
 
 var (
 
@@ -8,31 +8,31 @@ var (
 	// (m2*kg/sec2(Joule)) to kilowatt hour (kWh). The conversion from kWh
 	// to SI unit is 2.60E+06, so the inverse is used to convert from SI
 	// to kWh 1/2.60E+06 ~= 2.77777778e-7
-	SItoKWhPower = sdk.OutputType{
+	SItoKWhPower = output.Output{
 		Name:          "si-to-kwh.power",
 		Precision:     5,
 		ScalingFactor: "2.77777778e-7",
-		Unit: sdk.Unit{
+		Unit: &output.Unit{
 			Name:   "kilowatt hour",
 			Symbol: "kWh",
 		},
 	}
 
 	// Power is the output type for power (W) readings.
-	Power = sdk.OutputType{
+	Power = output.Output{
 		Name:      "power",
 		Precision: 3,
-		Unit: sdk.Unit{
+		Unit: &output.Unit{
 			Name:   "watt",
 			Symbol: "W",
 		},
 	}
 
 	// Microseconds in the output type for time readings in microseconds.
-	Microseconds = sdk.OutputType{
+	Microseconds = output.Output{
 		Name:      "microseconds",
 		Precision: 6,
-		Unit: sdk.Unit{
+		Unit: &output.Unit{
 			Name:   "microseconds",
 			Symbol: "µs",
 		},
@@ -41,32 +41,32 @@ var (
 	// FanSpeedPercent is the output type for the VEM PLC fan.
 	// This is a sliding window that is up to the PLC.
 	// We do not get absolute rpm.
-	FanSpeedPercent = sdk.OutputType{
+	FanSpeedPercent = output.Output{
 		Name:      "fan_speed_percent",
 		Precision: 3,
-		Unit: sdk.Unit{
+		Unit: &output.Unit{
 			Name:   "percent",
 			Symbol: "%",
 		},
 	}
 
 	// FanSpeedPercentTenths is the output type for the VEM PLC fan in tenths.
-	FanSpeedPercentTenths = sdk.OutputType{
+	FanSpeedPercentTenths = output.Output{
 		Name:          "fan_speed_percent_tenths",
 		Precision:     3,
 		ScalingFactor: ".1", // Raw reading for VEM PLC minimum fan speed is tenths of percent.
-		Unit: sdk.Unit{
+		Unit: &output.Unit{
 			Name:   "percent",
 			Symbol: "%",
 		},
 	}
 
 	// Temperature is the output type for temperature readings.
-	Temperature = sdk.OutputType{
+	Temperature = output.Output{
 		Name:          "temperature",
 		Precision:     3,
 		ScalingFactor: ".1", // Raw reading for VEM PLC is tenths of degrees F.
-		Unit: sdk.Unit{
+		Unit: &output.Unit{
 			Name:   "celsius",
 			Symbol: "C",
 		},
@@ -74,21 +74,21 @@ var (
 	}
 
 	// FlowGpm is the output type for flow readings in gallons per minute. FUTURE: Metric / English.
-	FlowGpm = sdk.OutputType{
+	FlowGpm = output.Output{
 		Name:      "flowGpm",
 		Precision: 4,
-		Unit: sdk.Unit{
+		Unit: &output.Unit{
 			Name:   "gallons per minute",
 			Symbol: "gpm",
 		},
 	}
 
 	// FlowGpmTenths is the output type for flow readings in tenths of gallons per minute. FUTURE: Metric / English.
-	FlowGpmTenths = sdk.OutputType{
+	FlowGpmTenths = output.Output{
 		Name:          "flowGpmTenths",
 		ScalingFactor: ".1",
 		Precision:     4,
-		Unit: sdk.Unit{
+		Unit: &output.Unit{
 			Name:   "gallons per minute",
 			Symbol: "gpm",
 		},
@@ -98,59 +98,67 @@ var (
 	// VEM PLC coils are all active high, but this may vary with different devices.
 	// Perhaps Synse should abstract this away and report all coils as active high
 	// because Sysnse is a device level abstraction layer(?)
-	Coil = sdk.OutputType{
+	Coil = output.Output{
 		Name:      "switch",
 		Precision: 1,
-		Unit: sdk.Unit{
+		Unit: &output.Unit{
 			Name:   "",
 			Symbol: "",
 		},
 	}
 
 	// InWCThousanths is the output type for pressure readings measured in thousanths of inches of water column..
-	InWCThousanths = sdk.OutputType{
+	InWCThousanths = output.Output{
 		Name:          "InWCThousanths",
 		ScalingFactor: ".001",
 		Precision:     4,
-		Unit: sdk.Unit{
+		Unit: &output.Unit{
 			Name:   "inches of water column",
 			Symbol: "InWC",
 		},
 	}
 
 	// PsiTenths is the output type for pressure readings measured in tenths of pounds per square inch..
-	PsiTenths = sdk.OutputType{
+	PsiTenths = output.Output{
 		Name:          "psiTenths",
 		ScalingFactor: ".1",
 		Precision:     3,
-		Unit: sdk.Unit{
+		Unit: &output.Unit{
 			Name:   "pounds per square inch",
 			Symbol: "psi",
 		},
 	}
 
 	// VoltSeconds is for flux.
-	VoltSeconds = sdk.OutputType{
+	VoltSeconds = output.Output{
 		Name:      "voltSeconds",
 		Precision: 3,
-		Unit: sdk.Unit{
+		Unit: &output.Unit{
 			Name:   "volt seconds",
 			Symbol: "Vs",
 		},
 	}
 
+	// FIXME: not clear that this belongs here, as this is Vapor-specific and not
+	//   modbus related. We could define a position output in the SDK.
 	// CarouselPosition is for the position of the Carousel, result is Wedge Id
 	// facing the customer.
-	CarouselPosition = sdk.OutputType{
+	CarouselPosition = output.Output{
 		Name:      "carouselPosition",
 		Precision: 3,
-		Unit:      sdk.Unit{Name: "position"},
+		Unit:      &output.Unit{
+			Name: "position",
+		},
 	}
 
 	// StatusCode is the integer assosciated with a status response.
-	StatusCode = sdk.OutputType{Name: "statusCode"}
+	StatusCode = output.Output{
+		Name: "statusCode",
+	}
 
 	// StatusOutput is for an arbitrary string output which is meant to be the
 	// string translation for status code.
-	StatusOutput = sdk.OutputType{Name: "status"}
+	StatusOutput = output.Output{
+		Name: "status",
+	}
 )
